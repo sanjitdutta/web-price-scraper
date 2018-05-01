315a818bc881fc8ac054c3212ad2b53334299629
Sanjit Dutta <sdlynx@gmail.com>
2018-05-01 08:51:21 +0800

    [service] added db compare, now just need to send emails

--- service/main.py
+++ service/main.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import logging
 
+import pymongo
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 
@@ -37,6 +38,12 @@ def _get_logger():
     logger.addHandler(ch)
     return logger
 
+def _notify_watchers(watch_obj):
+    """Sends email to watchers to watch stuff"""
+    logger = _get_logger()
+    logger.info("price has decreased - sending email(s)")
+    pass
+
 def main():
     """Main function"""
     logger = _get_logger()
@@ -54,25 +61,43 @@ def main():
 
     for watch in all_watches:
 
-        watch_id = watch["_id"]
-        watch_url = watch["url"]
-        watch_website_key = str(watch["website"])
-        watch_website_module = _WEBSITE_MAP[watch_website_key]
-
         watch_watchers = []
         for watcher in watch["watchers"]:
             watch_watchers.append(watcher["email"])
 
-        current_price = watch_website_module.scrape(watch_url)
-        logger.info("got price for watch with URL %s", watch_url)
+        watch_obj = {
+            "watch_id": watch["_id"],
+            "watch_url": watch["url"],
+            "watch_website_key": str(watch["website"]),
+            "watch_website_module": _WEBSITE_MAP[str(watch["website"])],
+            "watch_watchers": watch_watchers
+        }
+
+        last_watch_datum = watch_data.find_one(
+            filter={"watchKey": watch_obj["watch_id"]},
+            sort=[("date", pymongo.DESCENDING)]
+        )
+        last_watch_price = None
+
+        if last_watch_datum is not None:
+            last_watch_price = last_watch_datum["price"]
+            logger.info("got previous price %d for watch with URL %s", \
+                last_watch_price, watch_obj["watch_url"])
+
+        current_price = watch_obj["watch_website_module"].scrape(watch_obj["watch_url"])
+        logger.info("got price for watch with URL %s", watch_obj["watch_url"])
 
         watch_datum = {
-            "watchKey": watch_id,
-            "url": watch_url,
+            "watchKey": watch_obj["watch_id"],
+            "url": watch_obj["watch_url"],
             "price": current_price,
             "date": datetime.datetime.now(datetime.timezone.utc)
         }
         watch_data.insert_one(watch_datum)
-        logger.info("stored price %d to db for watch with URL %s", current_price, watch_url)
+        logger.info("stored price %d to db for watch with URL %s", \
+            current_price, watch_obj["watch_url"])
+
+        if last_watch_price is not None and current_price < last_watch_price:
+            _notify_watchers(watch_obj)
 
 main()
